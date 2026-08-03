1870258060f943ae303a20902b364bbd443721e8
smritisharma2924 <smritisharma2905@gmail.com>
2026-08-03 19:35:41 +0530

    Implement length-based text splitting

---
+++ text-splitters/1_length_based.py
@@ -0,0 +1,26 @@
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+text = """
+Space exploration has led to incredible scientific discoveries. 
+From landing on the Moon to exploring Mars, humanity continues to push the boundaries of what's possible beyond our planet.
+
+These missions have not only expanded our knowledge of the universe but have also contributed to advancements in technology here on Earth.
+Satellite communications, GPS, and even certain medical imaging techniques trace their roots back to innovations driven by space programs.
+"""
+
+loader = PyPDFLoader('text-splitters/aiml-curriculum.pdf')
+docs = loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 0,
+    separator = ''
+)
+
+result = splitter.split_text(text)
+print("Text: ", result)
+
+result2 = splitter.split_documents(docs)
+print("PDF: ", result2[0])
+print(len(result2))
